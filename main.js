6f50948831e313d1567b787c07c874a858316283
Heathcliff <dennis.nathan@gmail.com>
2016-08-04 11:18:28 -0600

    Fixed formatting so quote always appears beneath photo

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ $('document').ready(function () {
     url: 'https://yepi.io/api/image'
   }).done(function(image) {
     console.log(image);
-    $('#picture').append('<br><img src="' + image + '" /><br>')
+    $('#picture').append('<img src="' + image + '" />')
   })
 
   $.ajax ({
     url: 'https://yepi.io/api/quote'
   }).done(function(quote) {
-    $('#picture').append('<br><p>"' + quote + '"</p><br>');
+    $('#quote').append('<p>"' + quote + '"</p>');
   })
 })
